1ceb0cdb819aca67752dd9f8d997b412667c71fc
Flisan <me@loxygen.dev>
2022-09-25 16:54:59 +0900

    ✨ Implement primitive tester (#11)

---
+++ src/check/builtin/primitive.ts
@@ -0,0 +1,52 @@
+import { Definition } from "../../definitions";
+import {
+  declineTest,
+  testFailure,
+  TestResult,
+  testSuccess,
+} from "../test-result";
+import { NextFunction, ValueMatchTester } from "../tester";
+import { pipe } from "../utils/pipe-result";
+
+const availableTypes = ["string", "number", "bigint", "symbol"];
+const unavailableTypes = ["function", "object", "undefined"];
+const handlingTypes = [...availableTypes, ...unavailableTypes];
+
+export const primitiveDefinitionTester: ValueMatchTester = {
+  test(
+    definition: Definition,
+    testcase: unknown,
+    _next: NextFunction
+  ): TestResult {
+    if (typeof definition !== "string") {
+      return declineTest();
+    }
+
+    if (handlingTypes.indexOf(definition) === -1) {
+      return declineTest();
+    }
+
+    return pipe(() => testUnavailableTypes(definition))
+      .then(() => testCaseMatches(definition, testcase))
+      .evaluate();
+  },
+};
+
+function testUnavailableTypes(definition: Definition): TestResult {
+  const unavailableTypeUsed = unavailableTypes.indexOf(definition) !== -1;
+
+  return unavailableTypeUsed
+    ? testFailure(`The type '${definition}' cannot be used as a type.`, {
+        definition,
+      })
+    : testSuccess();
+}
+
+function testCaseMatches(definition: string, testcase: unknown): TestResult {
+  return typeof testcase === definition
+    ? testSuccess()
+    : testFailure(`Expected ${definition}, received ${typeof testcase}.`, {
+        definition,
+        testcase,
+      });
+}
